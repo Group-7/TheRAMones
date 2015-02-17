031cd008bb8f1e06f9275b394f3171b55c63e1b7
Andrzej Baba <andrzej.gda@gmail.com>
2015-02-17 18:39:41 +0000

    add displaying UE table in HTML (with script.js and other related files)

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,31 @@
+$("#infoBtn").click(function() {
+	
+	getAllUE();
+
+});
+
+function getAllUE(){
+	$.ajax({
+		  type: 'GET',
+		  url: 'rest/UE_Table/',
+		  success: handleResponseJQuery,
+		  contentType: 'application/json'
+	});
+	
+}
+
+function handleResponseJQuery(myData) {
+	
+	for(var i = 0 ; i <myData.length; i++){
+	
+		 $('#table-body').append(
+                 "<tr>" +
+                 "<td>" + myData[i].tac + "</td>" +
+                 "<td>" + myData[i].vendorName + "</td>" +
+                 "<td>" + myData[i].model + "</td>" +
+                 "<td>" + myData[i].ueType + "</td>" +
+                 "<td>" + myData[i].operatingSystem + "</td>" +
+                 "<td>" + myData[i].inputMode + "</td>" +
+                 "</tr>");
+             };
+}
