91fad83038776a3e478260ab1eda8df9500c9883
niallsull <niallsull@yahoo.co.uk>
2015-02-19 21:16:49 +0000

    Data file import

--- src/main/java/com/group7/rest/BaseDataRestService.java
+++ src/main/java/com/group7/rest/BaseDataRestService.java
@@ -1,27 +1,42 @@
 package com.group7.rest;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collection;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.httpclient.URI;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+
+import com.group7.entities.BaseData;
+import com.group7.entities.FileUploadForm;
+import com.group7.importBaseData.BaseDataExcelRead;
+import com.group7.serviceInterface.BaseDataService;
 
 import jxl.read.biff.BiffException;
 
-import com.group7.entities.BaseData;
-import com.group7.importBaseData.BaseDataExcelRead;
-import com.group7.serviceInterface.BaseDataService;
+
 
 @Path("/baseData")
 public class BaseDataRestService {
 
 	@Inject
 	private BaseDataService service;
+
+	
+	
 	
 	public BaseDataRestService(){
 		
@@ -42,5 +57,53 @@ public class BaseDataRestService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void addBaseData(BaseData basedata){
 		service.addBaseData(basedata);
+	}
+	
+	@POST
+	@Path("/upload")
+	@Consumes("multipart/form-data")
+	public void uploadFile(@MultipartForm FileUploadForm form,
+						@Context HttpServletResponse response) throws IOException{
+		
+		
+		
+		String filename="/home/niall/Data.xls";
+		if(form==null)
+			filename="null.txt";
+		
+		
+		try{
+			writeFile(form.getData(),filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		}
+		
+		
+		System.out.println("done");
+		
+		//response.sendRedirect("../../import.html");
+		
+		/*return Response.status(200)
+				.entity("uploadFile is called , Uploaded"
+						+ " file name : "+filename).build();*/
+	}
+	
+	private void writeFile(byte[] content, String fileName) throws IOException {
+		File file=new File(fileName);
+		
+		if(file.exists() == false){
+			file.createNewFile();
+		}
+		
+		
+		FileOutputStream fop = new FileOutputStream(file);
+		fop.write(content);
+		
+		fop.flush();
+		fop.close();
+		
+		
+		
 	}
 }
