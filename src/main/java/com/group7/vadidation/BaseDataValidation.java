72cec5a0c218d814a4b24a7111ae2ed9f4b99c3d
giovanni325 <d14125333@mydit.ie>
2015-02-20 14:59:16 +0000

    basedataValidation refactoring update

--- src/main/java/com/group7/vadidation/BaseDataValidation.java
+++ src/main/java/com/group7/vadidation/BaseDataValidation.java
@@ -5,50 +5,17 @@ import java.util.List;
 
 public class BaseDataValidation { 
 		
-	//private static String[] mccArray = new String[]{"238", "240", "302", "310", "340", "344", "405", "440", "505"};
 	private List<String> mncData = new ArrayList<String>();
 	private List<String> cellIdData = new ArrayList<String>();
 	
 	 
 	public BaseDataValidation(){
-		mncData.add("0");
-		mncData.add("1");
-		mncData.add("2");
-		mncData.add("3");
-		mncData.add("20");
-		mncData.add("21");
-		mncData.add("36");
-		mncData.add("37");
-		mncData.add("62");
-		mncData.add("63");
-		mncData.add("10");
-		mncData.add("12");
-		mncData.add("15");
-		mncData.add("540");
-		mncData.add("550");
-		mncData.add("560");
-		mncData.add("570");
-		mncData.add("580");
-		mncData.add("590");
-		mncData.add("30");
-		mncData.add("920");
-		mncData.add("930");
-		mncData.add("459");
-		mncData.add("11");;
-		mncData.add("68");
-		mncData.add("71");
-		mncData.add("72");
-		mncData.add("88");
-		mncData.add("90");
 		
-		cellIdData.add("4");
-		cellIdData.add("5");
-		cellIdData.add("3842");
-
+		// populate the lists with valid lookupTable data
+		buildValidValidationData();
 		
 	}
-	
-	
+		
 		/**
 		 * method that checks for a valid mnc code
 		 * 
@@ -103,6 +70,42 @@ public class BaseDataValidation {
 				}
 			return false;
 		}	
+		
+		public void buildValidValidationData(){
+			mncData.add("0");
+			mncData.add("1");
+			mncData.add("2");
+			mncData.add("3");
+			mncData.add("20");
+			mncData.add("21");
+			mncData.add("36");
+			mncData.add("37");
+			mncData.add("62");
+			mncData.add("63");
+			mncData.add("10");
+			mncData.add("12");
+			mncData.add("15");
+			mncData.add("540");
+			mncData.add("550");
+			mncData.add("560");
+			mncData.add("570");
+			mncData.add("580");
+			mncData.add("590");
+			mncData.add("30");
+			mncData.add("920");
+			mncData.add("930");
+			mncData.add("459");
+			mncData.add("11");;
+			mncData.add("68");
+			mncData.add("71");
+			mncData.add("72");
+			mncData.add("88");
+			mncData.add("90");
+			
+			cellIdData.add("4");
+			cellIdData.add("5");
+			cellIdData.add("3842");
+		}
 		
 		
 }
