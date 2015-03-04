284918688b8f60a015dae8e67ac20bca81fa02c3
Andrzej Baba <andrzej.gda@gmail.com>
2015-03-04 00:14:15 +0000

    created dropdown menu with unique imsi numbers for user story 4

--- src/main/webapp/js/eventid_causeid.js
+++ src/main/webapp/js/eventid_causeid.js
@@ -1,28 +1,56 @@
 //$("#infoBtn").click(function() {
-$(document).ready(function(){	
+$(document).ready(function() {
 	getAllEvents();
+	//createDropdownMenu();
 });
 
-function getAllEvents(){
+function getAllEvents() {
 	$.ajax({
-		  type: 'GET',
-		  url: 'rest/baseData/eventid_causeid/',
-		  success: handleResponseJQuery,
-		  contentType: 'application/json'
+		type : 'GET',
+		url : 'rest/baseData/eventid_causeid/',
+		success : handleResponseJQuery,
+		contentType : 'application/json'
 	});
-	
 }
+
 
 function handleResponseJQuery(myData) {
 	
-	for(var i = 0 ; i <myData.length; i++){
+	var select = document.getElementById("imsiNumber");
+	for(var i = 0; i < myData.length-1; i++) {
 	
-		 $('#table-body').append(
-                 "<tr>" +
-                 "<td>" + myData[i][0] + "</td>" +
-                 "<td>" + myData[i][1] + "</td>" +
-                 "<td>" + myData[i][2] + "</td>" +
-                 "</tr>");
-             };
-		
+		//separate unique numbers from the all imsi and paste to dropdown list
+		if(myData[i][0] != myData[i+1][0])
+		{
+			var opt = myData[i][0];
+		    var el = document.createElement("option");
+		    el.textContent = opt;
+		    el.value = opt;
+		    select.appendChild(el);
+		    continue;
+		}		
+	}
+	
+	//create table
+	for (var i = 0; i < myData.length; i++) {
+
+		$('#table-body').append(
+				"<tr>" + "<td>" + myData[i][0] + "</td>" + "<td>"
+						+ myData[i][1] + "</td>" + "<td>" + myData[i][2]
+						+ "</td>" + "</tr>");			
+	};
 }
+
+//function handleResponseJQuery(myData) {
+//
+//	for (var i = 0; i < myData.length; i++) {
+//
+//		$('#table-body').append(
+//				"<tr>" + "<td>" + myData[i][0] + "</td>" + "<td>"
+//						+ myData[i][1] + "</td>" + "<td>" + myData[i][2]
+//						+ "</td>" + "</tr>");
+//		
+//	}
+//	;
+//
+//}
