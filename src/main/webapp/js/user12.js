a5c2070aeca7658152caddda3128865117909680
marc28 <elmarco18@yahoo.ie>
2015-04-09 11:46:57 +0100

    More graph work

--- src/main/webapp/js/user12.js
+++ src/main/webapp/js/user12.js
@@ -33,7 +33,82 @@ function getResultsForTable(data){
 				"<td>"+data[i][1]+"</td>"+
 		"</tr>");
 	}
+	var doughnutData = [
+	    				{
+	    					value: data[0][1],
+	    					color:"#F7464A",
+	    					highlight: "#FF5A5E",
+	    					label: data[0][0]
+	    				},
+	    				{
+	    					value: data[1][1],
+	    					color: "#46BFBD",
+	    					highlight: "#5AD3D1",
+	    					label: data[1][0]
+	    				},
+	    				{
+	    					value: data[2][1],
+	    					color: "#FDB45C",
+	    					highlight: "#FFC870",
+	    					label: data[2][0]
+	    				},
+	    				{
+	    					value: data[3][1],
+	    					color: "#949FB1",
+	    					highlight: "#A8B3C5",
+	    					label: data[3][0]
+	    				},
+	    				{
+	    					value: data[4][1],
+	    					color: "#4D5360",
+	    					highlight: "#616774",
+	    					label: data[4][0]
+	    				},
+	    				{
+	    					value: data[5][1],
+	    					color: "#4D5360",
+	    					highlight: "#616774",
+	    					label: data[5][0]
+	    				}/*,
+	    				{
+	    					value: data[6][1],
+	    					color: "#675E60",
+	    					highlight: "#A34322",
+	    					label: data[6][0]
+	    				},
+	    				{
+	    					value: data[7][1],
+	    					color: "#3E7760",
+	    					highlight: "#B12334",
+	    					label: data[7][0]
+	    				},
+	    				{
+	    					value: data[8][1],
+	    					color: "#98C312",
+	    					highlight: "#166477",
+	    					label: data[8][0]
+	    				},
+	    				{
+	    					value: data[9][1],
+	    					color: "#DD3609",
+	    					highlight: "#AA2311",
+	    					label: data[9][0]
+	    				}*/
+	    			];
+	
+	var ctx = document.getElementById("chart-area").getContext("2d");
+	window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
 }
+
+
+
+$("#chart-area").click( 
+	    function(evt){
+	        var activePoints = myDoughnut.getSegmentsAtEvent(evt);           
+	        var url = "label= " + activePoints[0].label + "&value= " + activePoints[0].value;
+            alert(url);
+	    }
+	); 
 
 
 
